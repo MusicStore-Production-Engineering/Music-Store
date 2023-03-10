a7832ec91bf38d76661efc07b3385cb6eefeed2c
Guță George <58084950+Qubicon@users.noreply.github.com>
2023-03-10 07:05:51 +0000

    Added simple class logic for Category

---
+++ src/main/java/ro/unibuc/hello/dto/Category.java
@@ -0,0 +1,48 @@
+package ro.unibuc.hello.dto;
+import java.util.List;
+
+public class Category {
+    private int categoryId;
+    private String categoryName;
+    private String categoryDescription;
+    private List<Product> productList;
+
+    public Category(int categoryId, String categoryName, String categoryDescription, List<Product> productList) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.categoryDescription = categoryDescription;
+        this.productList = productList;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+}
